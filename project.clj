01b8ec8212430179b0cd93f7951a3a18cc566a56
Stanislav Vitko <stas@vitko.me>
2023-05-13 11:58:56 -0700

    setup for calva

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/vistrcm/sfweath"
   :license {:name "MIT"
             :url "https://github.com/vistrcm/sfweath/blob/main/LICENSE.md"}
-  :dependencies [
-                 [org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [enlive "1.1.6"]
                  [clj-http "3.12.3"]
                  [cheshire "5.11.0"]
                  [org.clojure/tools.reader "1.3.6"]]
+  :profiles {:repl {:plugins [[cider/cider-nrepl "0.30.0"]]}}
   :repl-options {:init-ns sfweath.core}
   :main sfweath.core)
